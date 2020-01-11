5fc3c5606cff36d5c6b0541227b2e325fa54af94
augusin11 <augusin11@gmail.com>
2020-01-11 15:56:36 -0800

    sort of finished login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,27 +4,42 @@ import {View, Button, Text, ScrollView, StyleSheet, Switch} from 'react-native'
 
 
 export default class LogInScreen extends React.Component {
-  static navigationOptions = {
-    title: 'LOGIN',
-    header: null
-  };
+
+    state = {
+        username: "",
+        password: ""
+    }
+
+    static navigationOptions = {
+        title: 'LOGIN',
+        header: null
+    };
 
 
-  render() {
-    return (
+    render() {
+        return (
+            <View style={{paddingBottom: 200}}>
+                <LoginScreen
+                    source={{
+                        uri: { backgroundColor: '#0000FF' }
+                    }}
 
+                    usernameOnChangeText={(username) => this.setState({
+                        username: username,
+                        password: this.state.password
+                    })}
 
-        <View style={{paddingBottom: 200}}>
-          <LoginScreen
-              source={{
-                uri: { backgroundColor: '#0000FF' }
-              }}
-              onPressLogin={() => alert("The username and the password you entered does not match.")}
-              logoText="Hacker Match"
-              logoComponent={null}
-              loginButtonBackgroundColor="#21a2f6"
-          />
-        </View>)
-  }
+                    passwordOnChangeText={(password) => this.setState({
+                        username: this.state.username,
+                        password: password
+                    })}
+                    onPressSettings={() => alert("U wanna sign up na")}
+                    onPressLogin={() => alert([this.state.username, this.state.password])}
+                    logoText="Hacker Match"
+                    logoComponent={null}
+                    loginButtonBackgroundColor="#21a2f6"
+                />
+            </View>)
+    }
 }
 
