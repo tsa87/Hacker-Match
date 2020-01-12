2dd6687f81029e55f15bc2acc1d888727f9c9857
augusin11 <augusin11@gmail.com>
2020-01-11 18:06:08 -0800

    Added StartScreen, but commented out for now

---
+++ screens/StartScreen.js
@@ -0,0 +1,46 @@
+// import React from "react";
+// import LoginScreen from "react-native-login-screen";
+// import {Dimensions, View, Text, ScrollView, StyleSheet, Switch} from 'react-native'
+// import { Button } from 'react-native-elements';
+//
+// const styles = StyleSheet.create({
+//         // center: {
+//         //   flex: 1,
+//         //   alignItems: 'center',
+//         //   justifyContent: 'center',
+//         // },
+//         buttonView: {
+//             alignItems:'center',
+//             justifyContent: 'center',
+//
+//             flex: 1
+//         },
+//         margin: {
+//             marginTop: 100,
+//         },
+//     }
+// )
+//
+// export default class StartScreen extends React.Component {
+//
+//     static navigationOptions = {
+//         title: 'Start',
+//         header: null
+//     };
+//
+//     render() {
+//         return (
+//             <View style={styles.buttonView}>
+//                 <Button
+//                     onPress={() => alert("yeet")}
+//                     title="Log In"
+//                 />
+//                 <Button style={styles.margin}
+//                         type="outline"
+//                         onPress={() => alert("yeet")}
+//                         title="New? Sign up now"
+//                 />
+//             </View>)
+//     }
+// }
+//
