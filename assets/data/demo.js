678167ef7bc707d633218ef683742167a897bd16
Tony Shen <tsa87@sfu.ca>
2020-01-12 11:25:04 -0800

    created custom characters

--- assets/data/demo.js
+++ assets/data/demo.js
@@ -1,15 +1,14 @@
 module.exports = [
 	{
 		id: 1,
-		name: 'Joey Shi',
-		status: 'Online',
-		match: '78',
+		name: 'Molly Ria',
+		match: '76',
 		description:
-			'Second-year student at UBC, Computer Scientist, Mathematician',
-		message:
-			'I will go back to Gotham and I will fight men Iike this but I will not become an executioner.',
-    skills: ["Python", "Java"],
-		image: require('../images/01.jpg')
+			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+		status: 'Offline',
+		message: "It's not who I am underneath but what I do that defines me.",
+    skills: ["Javascript", "React Native"],
+		image: require('../images/.jpg')
 	},
 	{
 		id: 2,
@@ -24,15 +23,15 @@ module.exports = [
 	},
 	{
 		id: 3,
-		name: 'Mr. Krabs',
-		match: '45',
+		name: 'Nicole Yasmin',
+		match: '95',
 		description:
 			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Offline',
+		status: 'Online',
 		message:
-			'Oh, hee-hee, aha. Ha, ooh, hee, ha-ha, ha-ha. And I thought my jokes were bad.',
-    skills: ["Python", "Java"],
-		image: require('../images/crab.jpg')
+			'You have nothing, nothing to threaten me with. Nothing to do with all your strength.',
+    skills: ["Machine Learning", "Python"],
+		image: require('../images/girl.jpg')
 	},
 	{
 		id: 4,
@@ -43,34 +42,23 @@ module.exports = [
 		status: 'Online',
 		message: "Bats frighten me. It's time my enemies shared my dread.",
     skills: ["Python", "Java"],
-		image: require('../images/10.jpg')
+		image: require('../images/aug.jpg')
 	},
 	{
 		id: 5,
-		name: 'James Dietrich',
-		match: '76',
+		name: 'Joey Shi',
+		status: 'Online',
+		match: '78',
 		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Offline',
-		message: "It's not who I am underneath but what I do that defines me.",
+			'Second-year student at UBC, Computer Scientist, Mathematician',
+		message:
+			'I will go back to Gotham and I will fight men Iike this but I will not become an executioner.',
     skills: ["Python", "Java"],
-		image: require('../images/05.jpg')
+		image: require('../images/01.jpg')
 	},
 	{
 		id: 6,
-		name: 'Patricia Schulist',
-		match: '95',
-		description:
-			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		status: 'Online',
-		message:
-			'You have nothing, nothing to threaten me with. Nothing to do with all your strength.',
-    skills: ["Python", "Java"],
-		image: require('../images/06.jpg')
-	},
-	{
-		id: 7,
-		name: 'Chelsey Weissnat',
+		name: 'Trevor Flanigan',
 		match: '67',
 		description:
 			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
@@ -78,29 +66,23 @@ module.exports = [
 		message:
 			"Never start with the head. The victim gets all fuzzy. He can't feel the next... See?",
     skills: ["Python", "Java"],
-		image: require('../images/07.jpg')
+		image: require('../images/trev.jpg')
 	},
 	{
-		id: 8,
-		name: 'Nicky Runol',
+		id: 7,
+		name: 'Balpreet Kalkat',
 		match: '85',
 		description:
 			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
-		age: '27',
-		location: 'Irvine, CA',
-		info1: 'Straight, Single, 5"10',
-		info2: 'Tea Totaller, Loves Photography & Travel',
-		info3: 'Beaches, Mountain, Cafe, Movies',
-		info4: 'Last seen: 23h ago',
 		status: 'Online',
 		message:
 			"And as for the television's so-called plan, Batman has no jurisdiction.",
-    skills: ["Python", "Java"],
+    skills: ["Python"],
 		image: require('../images/08.jpg')
 	},
 	{
-		id: 9,
-		name: 'Glenna Reichert',
+		id: 8,
+		name: 'Lucia Huan',
 		match: '74',
 		description:
 			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
@@ -108,7 +90,19 @@ module.exports = [
 		message:
 			'This is what happens when an unstoppable force meets an immovable object.',
     skills: ["Python", "Java"],
-		image: require('../images/09.jpg')
+		image: require('../images/girl2.jpg')
+	},
+	{
+		id: 9,
+		name: 'Mr. Krabs',
+		match: '45',
+		description:
+			'Full-time Traveller. Globe Trotter. Occasional Photographer. Part time Singer/Dancer.',
+		status: 'Offline',
+		message:
+			'Oh, hee-hee, aha. Ha, ooh, hee, ha-ha, ha-ha. And I thought my jokes were bad.',
+		skills: ["Python", "Java"],
+		image: require('../images/crab.jpg')
 	},
 	{
 		id: 10,
