200f21e0a6aeead877b159dce3de1945927ee903
augusin11 <augusin11@gmail.com>
2020-01-11 18:11:11 -0800

    updated App.js to include StartScreen at set it as initial screen

--- App.js
+++ App.js
@@ -3,30 +3,33 @@ import React from 'react';
 import SignInScreen from "./screens/SignInScreen"
 import SignUpScreen from "./screens/SignUpScreen"
 import SwipeScreen from "./screens/SwipeScreen"
+import StartScreen from "./screens/StartScreen"
+
 
 
 import {
-  createSwitchNavigator,
-  createAppContainer
+    createSwitchNavigator,
+    createAppContainer
 } from 'react-navigation';
 
 const MainSwitch = createSwitchNavigator(
-  {
-    Login: SignInScreen,
-    SignUp: SignUpScreen,
-    Swipe: SwipeScreen,
-  },
-  {
-    initialRouteName: "Login",
-  }
+    {
+        Login: SignInScreen,
+        SignUp: SignUpScreen,
+        Swipe: SwipeScreen,
+        Start: StartScreen
+    },
+    {
+        initialRouteName: "Start",
+    }
 )
 
 const Container = createAppContainer(MainSwitch)
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <Container />
-    )
-  }
+    render() {
+        return (
+            <Container />
+        )
+    }
 }
