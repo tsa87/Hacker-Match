7a119ea834c142ada9cad15df3a0ebf0ea8cfab9
Tony Shen <tsa87@sfu.ca>
2020-01-11 17:17:36 -0800

    added new assets

---
+++ assets/data/demo.js
@@ -0,0 +1,14 @@
+modele.export = [
+  {
+    id: 1,
+    firstName: "Tony",
+    lastName: "Shen",
+    profilePicture: require('../images/id1.jpg'),
+    github: "tsa87",
+    linkedin: "tony-shen-043754160".
+    location: "Vancouver",
+    skills: ["Python", "C++", "React Native", "Javascript"],
+    most_challenging:
+      "I once ate a whole pizza by myself."
+  },
+]
